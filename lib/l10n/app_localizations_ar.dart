44ac6ab5f5c128e31a2835645e7097e44160471d
Bassant <bassantsalama22@gmail.com>
2025-12-03 00:02:20 +0200

    Finish Splash Screen and Register Screen Ui + Localization

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,52 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get join_us => 'انضم إلينا!';
+
+  @override
+  String get register_qoute => 'ابدأ اليوم في بناء غدٍ أكثر إشراقًا لطفلك';
+
+  @override
+  String get child_name => 'اسم الطفل';
+
+  @override
+  String get age => 'العمر';
+
+  @override
+  String get parent_email => 'البريد الإلكتروني لولي الأمر';
+
+  @override
+  String get password => 'كلمة المرور';
+
+  @override
+  String get confirm_password => 'تأكيد كلمة المرور';
+
+  @override
+  String get email => 'البريد الإلكتروني';
+
+  @override
+  String get welcome_back => 'مرحباً بعودتك!';
+
+  @override
+  String get login => 'تسجيل الدخول';
+
+  @override
+  String get register => 'إنشاء حساب';
+
+  @override
+  String get dont_have_account => 'ليس لديك حساب؟ ';
+
+  @override
+  String get already_have_an_account => 'لديك حساب بالفعل ؟ ';
+
+  @override
+  String get create_one => 'أنشئ حساباً';
+}
---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,53 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get join_us => 'Join Us !';
+
+  @override
+  String get register_qoute =>
+      'Begin today to build a brighter tomorrow for you child .';
+
+  @override
+  String get child_name => 'Child Name';
+
+  @override
+  String get age => 'Age';
+
+  @override
+  String get parent_email => 'Parent Email';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get confirm_password => 'Confirm Password';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get welcome_back => 'Welcome Back !';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get register => 'Register';
+
+  @override
+  String get dont_have_account => 'Don\'t have an account ? ';
+
+  @override
+  String get already_have_an_account => 'Already have an account ? ';
+
+  @override
+  String get create_one => 'Create one';
+}
