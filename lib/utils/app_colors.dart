5588aa96832ec31a4dbca875c775cf1e5651dd59
Bassant <bassantsalama22@gmail.com>
2025-12-02 19:45:59 +0200

    Inilize Project

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color SoftBlue = Color (0xFF80A1BA);
+  static const Color MintGreen = Color (0xFFB4DEBD);
+  static const Color PastelPink = Color (0xFFF5CBCB);
+  static const Color LightPastelBlue = Color (0xFFBBDCE5);
+  static const Color BackgroundColor = Color (0xFFFAF7F3);
+  static const Color WhiteColor = Color (0xFFFFFF);
+  static const Color TrasparentColor = Colors.transparent;
+
+}
